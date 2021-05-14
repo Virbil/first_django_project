0f4c3ab9cc0c8a253e89549c39560c13fc262595
Bryce Eaton <bryce.eaton86@gmail.com>
2021-05-14 10:17:50 -0500

    Initial commit of multiple apps

---
+++ users_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register', views.register),
+    path('log-in', views.log_in),
+    path('new', views.register),
+    path('list-users', views.list_users),
+]
---
+++ users_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+
+def register(request):
+    if request == '/register':
+        return HttpResponse("placeholder for users to create a new user record")
+    else:
+        return HttpResponse("placeholder for users/new endpoint")
+
+
+def log_in(request):
+    return HttpResponse("placeholder for users to log in")
+
+def list_users(request):
+    return HttpResponse("placeholder to later display all the list of users")
