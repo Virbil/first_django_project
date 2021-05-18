af430a7965ed5a6fcb94adcb4f76f96397e0e6fe
Bryce Eaton <bryce.eaton86@gmail.com>
2021-05-17 21:04:20 -0500

    updated secrets

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"<Movie object: {self.title} ({self.id})>"
+
+
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
